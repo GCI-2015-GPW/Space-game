7f269ac113de0b84862a2eb454cc4072b2350a4b
Kinshuk <kinshukkashyap.1999@gmail.com>
2016-02-14 00:18:32 +0530

    Cleaned up undefined pointers, made code mode modular

--- main.cpp
+++ main.cpp
@@ -7,16 +7,18 @@ using namespace std;
 #define FPS 30
 #define WIN_SIZE 300, 300
 
-int main() {
+SDL_Window* window = NULL;
+SDL_Renderer* sdlRenderer = NULL;
 
+SDL_Event event;
+bool running = true;
+
+void init_SDL(){
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         cerr << "Unable to initialize SDL: " << SDL_GetError() << endl;
         exit(1);
     }
-
-    InitOpenGL();
-
-    SDL_Window *window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "Space Game",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -24,35 +26,50 @@ int main() {
         SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
     );
     SDL_MaximizeWindow(window);
-    SDL_Renderer *sdlRenderer = SDL_CreateRenderer(
+    sdlRenderer = SDL_CreateRenderer(
         window,
         -1,
         SDL_RENDERER_ACCELERATED);
+}
 
-    SDL_Event event;
-    bool running = true;
+void cleanup(){
+    SDL_DestroyWindow(window);
+    window = NULL;
+    SDL_Quit();
+}
 
-    while(running) {
-        int time = SDL_GetTicks();
+void mainloop(){
+    int time = SDL_GetTicks();
 
-        SDL_PumpEvents();
-        while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT) {
-                running = false;
-            }
-        }
-
-        glClearColor(1, 1, 1, 1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        SDL_GL_SwapWindow(window);
-
-        int ftime = SDL_GetTicks() - time;
-        int wait = (1000 / FPS) - ftime;
-        if(wait >= 0) {
-            SDL_Delay(wait);
+    SDL_PumpEvents();
+    while(SDL_PollEvent(&event)) {
+        if(event.type == SDL_QUIT) {
+            running = false;
         }
     }
 
-    SDL_Quit();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    SDL_GL_SwapWindow(window);
+
+    int ftime = SDL_GetTicks() - time;
+    int wait = (1000 / FPS) - ftime;
+    if(wait >= 0) {
+        SDL_Delay(wait);
+    }
+}
+
+int main() {
+    InitOpenGL();
+    init_SDL();    
+
+    
+
+    while(running) {
+        mainloop();
+    }
+
+    cleanup();
+    
     return 0;
 }
