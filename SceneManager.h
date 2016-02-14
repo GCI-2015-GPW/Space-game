0795945734bcc874cfc50418608a7e67ab2af3b8
Russell Greene <russellgreene8@gmail.com>
2016-02-13 19:50:17 -0700

    Add GLM and a few components

--- SceneManager.h
+++ SceneManager.h
@@ -2,4 +2,29 @@
 
 #include <Manager.h>
 
+#include <glm/vec3.hpp>
 
+#include <boost/signals2.hpp>
+
+
+// COMPONENTS
+struct CLocation
+{
+    glm::vec3 value;
+};
+
+struct CVelocity
+{
+    
+};
+
+using SceneManager_t = 
+    Manager<
+        decltype(make_type_tuple<CLocation, CVelocity>)
+    >;
+
+template<>
+struct ManagerData<SceneManager_t>
+{
+    boost::signals2::signal<void(float)> tick;
+};
